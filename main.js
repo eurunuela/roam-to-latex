339f5f867d8393e695d18aa8d98d570c2c5e4806
Eneko <enekouru@gmail.com>
2021-03-15 18:58:03 +0100

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+let pageTitle = document.title;
 
+getChildren = async (pageTitle)=> {
+    var foo = await window.roamAlphaAPI.q(`[:find (pull ?e [ :node/title :block/string :block/children {:block/children ...} ]) :where [?e :node/title "${pageTitle}"] ]`);
+    return foo;
+}
+
+alert(getChildren)
